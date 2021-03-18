7ed478d94ff0d7b4a9efa4010a42954e2a257201
Xiaochen Xu <xiaochex@gmail.com>
2021-03-17 23:16:08 -0400

    commit tree 8ab9ee48180d5ecce4dbb23df443c9f013761abe

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer(name){
+    bestCustomer = name;
+}
+
+const leastFavoriteCustomer = "Tim";
+function changeLeastFavoriteCustomer(name){
+    leastFavoriteCustomer = name;
+}
